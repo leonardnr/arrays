0cdf3022254156197b20dec43cfee5191c218226
leonardnr <leonardnr@s.dcsdk12.org>
2013-01-29 08:02:13 -0700

    Added threes method

--- arrays.java
+++ arrays.java
@@ -11,6 +11,7 @@ public class arrays{
 
 	public static void main(String[] args) {
 		int[] nums = {3, 6, 4, 3, 2, 1, 5, 6};
+		System.out.println(threes(nums));
 
 		System.out.println(avg(nums));
 	}
@@ -40,6 +41,24 @@ public class arrays{
 		
 		for(int i = 0; i < nums.length; i++){
 			counter += nums[i];
+		}
+
+		return counter;
+	}
+	/**
+	 * threes
+	 * 		Finds the number of 3's in an array
+	 *
+	 * @param nums - the array
+	 * @return the number of 3's in the array
+	 */
+	public static int threes(int[] nums){
+		int counter = 0;
+
+		for(int i = 0; i < nums.length; i++){
+			if(nums[i] == 3){
+				counter++;
+			}
 		}
 
 		return counter;
